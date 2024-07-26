eac2673334df2d49ccc4c9f2475edf1e86804a20
burakcankrpnr <burikcankorpinar7@gmail.com>
2024-07-26 15:58:58 +0300

    init

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import Login from "./components/Login";
-import Home from "./Home";
+import Home from "./components/Home";
 import ProtectedRoute from "./components/ProtectedRoutes";
 
 function App() {
