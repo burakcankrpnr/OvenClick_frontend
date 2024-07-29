99b8877ec89bdfac02ff8ad98735a66e1daddf8d
burakcankrpnr <burikcankorpinar7@gmail.com>
2024-07-29 09:44:19 +0300

    init

--- src/components/AuthContext.js
+++ src/components/AuthContext.js
@@ -3,7 +3,9 @@ import React, { createContext, useContext, useState } from "react";
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    !!localStorage.getItem("token")
+  );
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "./AuthContext"; // AuthContext'i içe aktar
 
 function Home() {
+  const navigate = useNavigate();
+  const { setIsAuthenticated } = useAuth();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsAuthenticated(false);
+    navigate("/login");
+  };
+
   return (
     <div>
       <h2>Home Page</h2>
       <p>Welcome to the home page!</p>
+      <button onClick={handleLogout}>Çıkış Yap</button>
     </div>
   );
 }
