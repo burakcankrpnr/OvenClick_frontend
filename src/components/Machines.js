26f225da5212a59b20f2ee94bb21a79eda7c20d2
burakcankrpnr <burikcankorpinar7@gmail.com>
2024-07-31 17:48:13 +0300

    init

--- src/components/Machines.js
+++ src/components/Machines.js
@@ -1,12 +1,24 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { FaCircle, FaDesktop, FaCalendarAlt } from "react-icons/fa";
+import {
+  FaCircle,
+  FaDesktop,
+  FaCalendarAlt,
+  FaPlus,
+  FaEdit,
+  FaTrash,
+} from "react-icons/fa";
 import "../styles/Machines.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 const baseURL = "http://localhost:3001";
 
 const Machines = ({ authToken }) => {
   const [machines, setMachines] = useState([]);
+  const [showAddForm, setShowAddForm] = useState(false);
+  const [newMachine, setNewMachine] = useState({ machine_name: "" });
+  const [editMachine, setEditMachine] = useState(null);
+  const [showEditForm, setShowEditForm] = useState(false);
 
   useEffect(() => {
     const fetchMachines = async () => {
@@ -14,7 +26,7 @@ const Machines = ({ authToken }) => {
         const response = await axios.get(baseURL + "/machines", {
           headers: { Authorization: `Bearer ${authToken}` },
         });
-        console.log("Makineler:", response);
+        console.log("Makineler:", response.data);
         setMachines(response.data);
       } catch (error) {
         console.error("Makineler alınırken hata:", error);
@@ -24,8 +36,159 @@ const Machines = ({ authToken }) => {
     fetchMachines();
   }, [authToken]);
 
+  const handleAddMachine = async () => {
+    if (
+      newMachine.machine_name.trim() === "" ||
+      newMachine.owner_id.trim() === "" ||
+      newMachine.details.trim() === ""
+    ) {
+      alert("Makine adı, sahip ID'si ve detaylar boş olamaz!");
+      return;
+    }
+    try {
+      console.log("Eklenecek makine:", newMachine);
+      const response = await axios.post(`${baseURL}/machines`, newMachine, {
+        headers: { Authorization: `Bearer ${authToken}` },
+      });
+      console.log("Makine eklendi:", response.data);
+      setNewMachine({ machine_name: "", owner_id: "", details: "" });
+      setShowAddForm(false);
+      const fetchResponse = await axios.get(baseURL + "/machines", {
+        headers: { Authorization: `Bearer ${authToken}` },
+      });
+      setMachines(fetchResponse.data);
+    } catch (error) {
+      console.error("Makine eklenirken hata:", error);
+    }
+  };
+
+  const handleEditMachine = async () => {
+    if (editMachine) {
+      try {
+        console.log("Güncellenecek makine:", editMachine);
+        const response = await axios.put(
+          `${baseURL}/machines/${editMachine.machine_id}`,
+          editMachine,
+          {
+            headers: { Authorization: `Bearer ${authToken}` },
+          }
+        );
+        console.log("Makine güncellendi:", response.data);
+        setEditMachine(null);
+        setShowEditForm(false);
+        const fetchResponse = await axios.get(baseURL + "/machines", {
+          headers: { Authorization: `Bearer ${authToken}` },
+        });
+        setMachines(fetchResponse.data);
+      } catch (error) {
+        console.error("Makine güncellenirken hata:", error);
+      }
+    }
+  };
+
+  const handleDeleteMachine = async (machineId) => {
+    try {
+      console.log("Silinecek makine ID:", machineId);
+      await axios.delete(`${baseURL}/machines/${machineId}`, {
+        headers: { Authorization: `Bearer ${authToken}` },
+      });
+      setMachines(
+        machines.filter((machine) => machine.machine_id !== machineId)
+      );
+    } catch (error) {
+      console.error("Makine silinirken hata:", error);
+    }
+  };
+
   return (
     <div className="container-fluid">
+      <h1>Machines</h1>
+      <div className="d-flex justify-content-between mb-4">
+        <button
+          className="btn btn-success"
+          onClick={() => setShowAddForm(!showAddForm)}
+        >
+          <FaPlus /> {showAddForm ? "Cancel" : "Add Machine"}
+        </button>
+      </div>
+
+      {showAddForm && (
+        <div className="add-machine-form">
+          <h2>Add New Machine</h2>
+          <input
+            type="text"
+            placeholder="Machine Name"
+            value={newMachine.machine_name}
+            onChange={(e) =>
+              setNewMachine({ ...newMachine, machine_name: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="Owner ID"
+            value={newMachine.owner_id || ""}
+            onChange={(e) =>
+              setNewMachine({ ...newMachine, owner_id: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="Details"
+            value={newMachine.details || ""}
+            onChange={(e) =>
+              setNewMachine({ ...newMachine, details: e.target.value })
+            }
+          />
+          <button className="btn btn-primary" onClick={handleAddMachine}>
+            Add
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => setShowAddForm(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      )}
+      {showEditForm && editMachine && (
+        <div className="edit-machine-form">
+          <h2>Edit Machine</h2>
+          <input
+            type="text"
+            placeholder="Machine Name"
+            value={editMachine.machine_name}
+            onChange={(e) =>
+              setEditMachine({ ...editMachine, machine_name: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="Owner ID"
+            value={editMachine.owner_id || ""}
+            onChange={(e) =>
+              setEditMachine({ ...editMachine, owner_id: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="Details"
+            value={editMachine.details || ""}
+            onChange={(e) =>
+              setEditMachine({ ...editMachine, details: e.target.value })
+            }
+          />
+          <button className="btn btn-primary" onClick={handleEditMachine}>
+            Update
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => setShowEditForm(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      )}
+
       <div className="row">
         {machines.map((machine) => (
           <div
@@ -45,10 +208,6 @@ const Machines = ({ authToken }) => {
                   <FaCalendarAlt className="mr-2" />
                   Updated Date: {new Date(machine.updated_at).toLocaleString()}
                 </p>
-                {/* <p className="card-text">
-                  <FaInfoCircle className="mr-2" />
-                  Details
-                </p> */}
                 <p className="card-text status-text">
                   <FaCircle
                     className={`mr-2 ${
@@ -57,6 +216,23 @@ const Machines = ({ authToken }) => {
                   />
                   {machine.actions ? "Online" : "Offline"}
                 </p>
+                <div className="action-buttons">
+                  <button
+                    className="btn btn-warning action-button"
+                    onClick={() => {
+                      setEditMachine(machine);
+                      setShowEditForm(true);
+                    }}
+                  >
+                    <FaEdit />
+                  </button>
+                  <button
+                    className="btn btn-danger action-button"
+                    onClick={() => handleDeleteMachine(machine.machine_id)}
+                  >
+                    <FaTrash />
+                  </button>
+                </div>
               </div>
             </div>
           </div>
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { FaEdit, FaTrash } from "react-icons/fa";
+import { FaEdit, FaTrash, FaPlus } from "react-icons/fa";
 import "../styles/Users.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -8,6 +8,15 @@ const baseURL = "http://localhost:3001";
 
 const Users = ({ token }) => {
   const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState({
+    username: "",
+    password: "",
+    role: "",
+    email: "",
+  });
+  const [showAddUserForm, setShowAddUserForm] = useState(false);
+  const [editUser, setEditUser] = useState(null);
+  const [showEditForm, setShowEditForm] = useState(false);
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -28,9 +37,170 @@ const Users = ({ token }) => {
     fetchUsers();
   }, [token]);
 
+  const handleDelete = async (userId) => {
+    try {
+      await axios.delete(`${baseURL}/user/${userId}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setUsers(users.filter((user) => user.user_id !== userId));
+    } catch (error) {
+      console.error(
+        "Kullanıcı silinirken hata:",
+        error.response ? error.response.data : error.message
+      );
+    }
+  };
+
+  const handleAddUser = async () => {
+    try {
+      await axios.post(`${baseURL}/user`, newUser, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setNewUser({ username: "", password: "", role: "", email: "" });
+      setShowAddUserForm(false);
+      const response = await axios.get(`${baseURL}/user`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setUsers(response.data);
+    } catch (error) {
+      console.error(
+        "Kullanıcı eklenirken hata:",
+        error.response ? error.response.data : error.message
+      );
+    }
+  };
+
+  const handleEditUser = async () => {
+    if (editUser) {
+      try {
+        await axios.put(`${baseURL}/user/${editUser.user_id}`, editUser, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        setEditUser(null);
+        setShowEditForm(false);
+        const response = await axios.get(`${baseURL}/user`, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        setUsers(response.data);
+      } catch (error) {
+        console.error(
+          "Kullanıcı güncellenirken hata:",
+          error.response ? error.response.data : error.message
+        );
+      }
+    }
+  };
+
+  const handleEditButtonClick = (user) => {
+    setEditUser(user);
+    setShowEditForm(true);
+  };
+
   return (
     <div className="users-container">
       <h1>Users</h1>
+      <button
+        className="add-user-button"
+        onClick={() => setShowAddUserForm(!showAddUserForm)}
+      >
+        <FaPlus /> {showAddUserForm ? "Cancel" : "Add User"}
+      </button>
+      {showAddUserForm && (
+        <div className="add-user-form-container">
+          <h2>Add New User</h2>
+          <input
+            type="text"
+            placeholder="Username"
+            value={newUser.username}
+            onChange={(e) =>
+              setNewUser({ ...newUser, username: e.target.value })
+            }
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={newUser.password}
+            onChange={(e) =>
+              setNewUser({ ...newUser, password: e.target.value })
+            }
+          />
+          <select
+            value={newUser.role}
+            onChange={(e) => setNewUser({ ...newUser, role: e.target.value })}
+          >
+            <option value="">Select Role</option>
+            <option value="Admin">Admin</option>
+            <option value="Official">Official</option>
+            <option value="Intern">Intern</option>
+            <option value="Manager">Manager</option>
+            <option value="Worker">Worker</option>
+          </select>
+          <input
+            type="email"
+            placeholder="Email"
+            value={newUser.email}
+            onChange={(e) => setNewUser({ ...newUser, email: e.target.value })}
+          />
+          <button className="add-button" onClick={handleAddUser}>
+            Add
+          </button>
+          <button
+            className="cancel-button"
+            onClick={() => setShowAddUserForm(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      )}
+      {showEditForm && editUser && (
+        <div className="edit-user-form-container">
+          <h2>Edit User</h2>
+          <input
+            type="text"
+            placeholder="Username"
+            value={editUser.username}
+            onChange={(e) =>
+              setEditUser({ ...editUser, username: e.target.value })
+            }
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={editUser.password}
+            onChange={(e) =>
+              setEditUser({ ...editUser, password: e.target.value })
+            }
+          />
+          <select
+            value={editUser.role}
+            onChange={(e) => setEditUser({ ...editUser, role: e.target.value })}
+          >
+            <option value="">Select Role</option>
+            <option value="Admin">Admin</option>
+            <option value="Official">Official</option>
+            <option value="Intern">Intern</option>
+            <option value="Manager">Manager</option>
+            <option value="Worker">Worker</option>
+          </select>
+          <input
+            type="email"
+            placeholder="Email"
+            value={editUser.email}
+            onChange={(e) =>
+              setEditUser({ ...editUser, email: e.target.value })
+            }
+          />
+          <button className="update-button" onClick={handleEditUser}>
+            Update
+          </button>
+          <button
+            className="cancel-button"
+            onClick={() => setShowEditForm(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      )}
       <table className="users-table">
         <thead>
           <tr>
@@ -55,10 +225,16 @@ const Users = ({ token }) => {
               <td>{new Date(user.created_at).toLocaleString()}</td>
               <td>{new Date(user.updated_at).toLocaleString()}</td>
               <td className="action-buttons">
-                <button className="edit-button">
+                <button
+                  className="edit-button"
+                  onClick={() => handleEditButtonClick(user)}
+                >
                   <FaEdit />
                 </button>
-                <button className="delete-button">
+                <button
+                  className="delete-button"
+                  onClick={() => handleDelete(user.user_id)}
+                >
                   <FaTrash />
                 </button>
               </td>
